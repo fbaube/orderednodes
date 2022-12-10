a1174a9b781812deae66722059a623a055c6a0c4
Fred Baube <fbaube@welho.com>
2022-12-10 20:42:13 +0200

    Move & expand (to css) print funcs

--- nord.go
+++ nord.go
@@ -3,10 +3,8 @@ package orderednodes
 import (
 	"fmt"
 	L "github.com/fbaube/mlog"
-	"io"
 	"os"
 	FP "path/filepath"
-	S "strings"
 
 	FU "github.com/fbaube/fileutils"
 )
@@ -227,95 +225,4 @@ func (pOld *Nord) ReplaceWith(pNew Norder) Norder {
 	// REPLACE PARENT'S KID-LINK
 
 	return pNew
-}
-
-// Echo implements Markupper.
-func (p *Nord) Echo() string {
-	panic("recursion") // return p.Echo()
-}
-
-// LinePrefixString provides indentation and should start a line of display/debug.
-// It does not end the string with (white)space.
-func (p Nord) LinePrefixString() string {
-	if p.isRoot { // && p.Parent == nil
-		return "[R]"
-	} else if p.Level() == 0 && p.Parent() != nil {
-		return fmt.Sprintf("[%d]", p.seqID)
-	} else {
-		// (spaces)[lvl:seq]"
-		// func S.Repeat(s string, count int) string
-		return fmt.Sprintf("%s[%02d:%02d]",
-			S.Repeat("  ", p.level-1), p.level, p.seqID)
-	}
-}
-
-func yn(b bool) string {
-	if b {
-		return "Y"
-	} else {
-		return "n"
-	}
-}
-
-// func (p *Nord) NordSummaryString() string {
-func (p *Nord) LineSummaryString() string {
-	var sb S.Builder
-	if p.IsRoot() {
-		sb.WriteString("ROOT ")
-	}
-	/*
-		if p.PrevKid() != nil {
-			sb.WriteString("P ")
-		}
-		if p.Parent() == nil {
-			sb.WriteString("NOPARENT ")
-		}
-		if p.NextKid() != nil {
-			sb.WriteString("N ")
-		}
-		if p.HasKids() {
-			sb.WriteString("kid(s) ")
-		}
-	*/
-	if p.path == "" {
-		sb.WriteString("NOPATH")
-	} else {
-		sb.WriteString(p.path)
-	}
-	return (sb.String())
-}
-
-var printAllTo io.Writer
-
-func init() {
-	f, e := os.Create("./contentity-tree")
-	if e == nil {
-		printAllTo = f
-	} else {
-		println("contentity-tree:", e.Error())
-	}
-}
-
-func (p *Nord) PrintTree(w io.Writer) error {
-	// println("PrintTree: could use printer fn")
-	if w == nil {
-		return nil
-	}
-	printAllTo = w
-	e := InspectTree(p, nordPrintOneLiner)
-	if e != nil {
-		println("nordPrintOneLiner ERR:", e.Error())
-		return e
-	}
-	return nil
-}
-
-func nordPrintOneLiner(p Norder) error {
-	// var F StringFunc
-	// F = p.GetLineSummaryFunc()
-	// fmt.Fprintf(printAllTo, "%s %s (%T) \n", p.LinePrefixString(), F(p), p)
-	// fmt.Fprintf(printAllTo, "%s %s (%T) \n", p.LinePrefixString(), "?", p)
-	fmt.Fprintf(printAllTo, "%s %s \n",
-		p.LinePrefixString(), p.LineSummaryString())
-	return nil
 }
